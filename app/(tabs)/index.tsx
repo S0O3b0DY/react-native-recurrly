b58dc74f1cccdc71d867fba696f7add56863134b
BuildTools <unconfigured@null.spigotmc.org>
2026-04-07 09:48:59 +0200

    Implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,19 +7,12 @@ const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to sign in</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to sign up</Link>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Go to Onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Go to sign in</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4 font-sans-bold">Go to sign up</Link>
 
-      <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">Spotify subscription</Link>
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: { id: "claude" } 
-      }} className="mt-4 rounded bg-primary text-white p-4">Claude max subscription</Link>
     </SafeAreaView>
   )
 }
